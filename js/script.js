7aa305dcea4b0674970f067df0a2be1c326d3198
今村一貴 <imamurakazuki@imamurakazutakanoMacBook-Pro.local>
2021-01-18 00:11:11 +0900

    add js/script.js to raise tech

--- js/script.js
+++ js/script.js
@@ -1,10 +1,21 @@
-$(".dropdown-btn").hover(
-  function() {
-    $(".dropdown-menu", this).stop().slideDown(200);
-    $(this).addClass('dropdown-btn-open');
-  },
-  function() {
-    $(".dropdown-menu", this).stop().slideUp(100);
-    $(this).removeClass('dropdown-btn-open');
-  }
-);
+$(function(){
+
+
+
+
+  $(".dropdown-btn").hover(
+    function() {
+      $(".dropdown-menu", this).stop().slideDown(200);
+      $(this).addClass('dropdown-btn-open');
+    },
+    function() {
+      $(".dropdown-menu", this).stop().slideUp(100);
+      $(this).removeClass('dropdown-btn-open');
+    }
+  );
+
+
+
+
+})
+
