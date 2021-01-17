0f1f5d753450662fe9334d0b92ce105b2f9f0fb4
今村一貴 <imamurakazuki@imamurakazutakanoMacBook-Pro.local>
2021-01-18 00:48:19 +0900

    js/script.js to raise tech

--- js/script.js
+++ js/script.js
@@ -1,15 +1,13 @@
 $(function(){
 
 
-
-
-  $(".dropdown").hover(
+  $(".dropdown-btn").hover(
     function() {
       $(".dropdown-menu", this).stop().slideDown(200);
       $(this).addClass('dropdown-btn-open');
     },
     function() {
-      $(".dropdown", this).stop().slideUp(100);
+      $(".dropdown-btn", this).stop().slideUp(100);
       $(this).removeClass('dropdown-btn-open');
     }
   );
